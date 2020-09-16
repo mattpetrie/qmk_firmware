9edec58adb7a224030983cd6eb81983fd6b8af62
Matt Petrie <matthew.petrie@gmail.com>
2020-09-16 10:43:15 -0700

    Require only 2 taps for toggling layers

--- keyboards/dztech/dz60rgb_ansi/v2/config.h
+++ keyboards/dztech/dz60rgb_ansi/v2/config.h
@@ -38,6 +38,9 @@
 #define NO_ACTION_MACRO
 #define NO_ACTION_FUNCTION
 
+/* Only require 2 taps for toggling layers */
+#define TAPPING_TOGGLE 2
+
 #ifdef RGB_MATRIX_ENABLE
 #    define RGB_DISABLE_AFTER_TIMEOUT 0          // number of ticks to wait until disabling effects
 #    define RGB_DISABLE_WHEN_USB_SUSPENDED true  // turn off effects when suspended
